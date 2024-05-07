ca81fb4ad750a2fb75787ac88aae28065fe7792b
HSH <han41562@gmail.com>
2024-05-07 11:42:54 +0900

    build: DB 테스트용 파일 삭제

--- src/main/java/com/example/spinlog/dev/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.spinlog.dev;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
