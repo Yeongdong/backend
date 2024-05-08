ccb661b1ad702b5a1d5d89f29b9a3f7cfc82e038
HSH <han41562@gmail.com>
2024-05-08 19:43:06 +0900

    feat: 성별 통계 API 위한 GenderStatisticsController 기능 구현

--- src/main/java/com/example/spinlog/statistics/controller/GenderStatisticsController.java
+++ src/main/java/com/example/spinlog/statistics/controller/GenderStatisticsController.java
@@ -1,5 +1,8 @@
 package com.example.spinlog.statistics.controller;
 
+import com.example.spinlog.article.entity.RegisterType;
+import com.example.spinlog.statistics.repository.dto.GenderSatisfactionAverageDto;
+import com.example.spinlog.statistics.service.GenderStatisticsService;
 import com.example.spinlog.statistics.service.dto.GenderDailyAmountSumResponse;
 import com.example.spinlog.statistics.service.dto.GenderEmotionAmountAverageResponse;
 import com.example.spinlog.statistics.service.dto.GenderWordFrequencyResponse;
@@ -9,32 +12,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class GenderStatisticsController {
+    private final GenderStatisticsService genderStatisticsService;
     @GetMapping("/api/statistics/gender/emotion/amounts/average")
     public List<GenderEmotionAmountAverageResponse> getAmountAveragesEachGenderAndEmotionLast90Days(
             @RequestParam(defaultValue = "SPEND") String registerType){
-        return null;
+        return genderStatisticsService.getAmountAveragesEachGenderAndEmotionLast90Days(
+                LocalDate.now(),
+                RegisterType.valueOf(registerType));
     }
 
     @GetMapping("/api/statistics/gender/daily/amounts/sum")
     public List<GenderDailyAmountSumResponse> getAmountSumsEachGenderAndDayLast90Days(
             @RequestParam(defaultValue = "SPEND") String registerType){
-        return null;
+        return genderStatisticsService.getAmountSumsEachGenderAndDayLast90Days(
+                LocalDate.now(),
+                RegisterType.valueOf(registerType));
     }
 
     @GetMapping("/api/statistics/gender/word/frequencies")
-    public List<GenderWordFrequencyResponse> getWordFrequencyEachGenderLast90Days(){
-        return null;
+    public GenderWordFrequencyResponse getWordFrequencyEachGenderLast90Days(){
+        return genderStatisticsService.getWordFrequenciesLast90Days(LocalDate.now());
     }
 
     @GetMapping("/api/statistics/gender/statisfactions/average")
-    public List<Object> getStatisfactionAveragesEachGenderLast90Days(
+    public List<GenderSatisfactionAverageDto> getStatisfactionAveragesEachGenderLast90Days(
             @RequestParam(defaultValue = "SPEND") String registerType){
-        return null;
+        return genderStatisticsService.getSatisfactionAveragesEachGenderLast90Days(
+                LocalDate.now(),
+                RegisterType.valueOf(registerType));
     }
 }
