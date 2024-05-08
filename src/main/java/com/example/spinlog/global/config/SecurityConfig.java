9879a9aeae1707dd98d7d21ca8ae13bd934b6c57
황중섭 <jshwang9709@gmail.com>
2024-05-09 02:55:19 +0900

    feat: OAuth2 access token 저장 방식을 기본 인메모리 방식에서 DB 로 변경 (#4)

--- src/main/java/com/example/spinlog/global/config/SecurityConfig.java
+++ src/main/java/com/example/spinlog/global/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.spinlog.global.config;
 
 import com.example.spinlog.global.config.oauth2.CustomClientRegistrationRepository;
+import com.example.spinlog.global.config.oauth2.CustomOAuth2AuthorizedClientService;
 import com.example.spinlog.user.security.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,10 +18,11 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomClientRegistrationRepository customClientRegistrationRepository;
+    private final CustomOAuth2AuthorizedClientService customOAuth2AuthorizedClientService;
+    private final JdbcTemplate jdbcTemplate;
 
     /*
     TODO oauth2Login() 관련
-        2. 로그인 이후의 access token 과 refresh token 등을 저장하는 방식을 기본 인메모리 방식에서 DB 로 변경
         3. 로그아웃을 한 뒤 다시 로그인을 하면 자동 로그인이 되지 않게
         4. 최초 로그인인지 판별
         5. cors 설정
@@ -38,6 +41,9 @@ public class SecurityConfig {
                 )
                 .oauth2Login(oauth2 -> oauth2
                         .clientRegistrationRepository(customClientRegistrationRepository.clientRegistrationRepository())
+                        .authorizedClientService(customOAuth2AuthorizedClientService.oAuth2AuthorizedClientService(
+                                jdbcTemplate, customClientRegistrationRepository.clientRegistrationRepository()
+                        ))
                         .userInfoEndpoint(userInfoEndpointConfig ->
                                 userInfoEndpointConfig.userService(customOAuth2UserService))
 
