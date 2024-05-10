fa1d778e28c82e700957701a0dcff5db18ecdd63
seop-h <jshwang9709@gmail.com>
2024-05-10 19:21:18 +0900

    refactor: User 엔티티 변경 (#4)
    - Article 연관관게 매핑 (OneToMany)
    - age 필드 삭제

--- src/main/java/com/example/spinlog/user/entity/User.java
+++ src/main/java/com/example/spinlog/user/entity/User.java
@@ -1,6 +1,8 @@
 package com.example.spinlog.user.entity;
 
+import com.example.spinlog.article.entity.Article;
 import com.example.spinlog.global.entity.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,6 +10,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -15,6 +18,9 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -29,9 +35,6 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false)
     private String email;
 
-    @Min(1) @Max(100) //TODO validation 코드를 entity 에 넣어도 되는지
-    private Integer age;
-
     @Enumerated(EnumType.STRING) //TODO DB 에서 디폴트값 NONE 으로 설정
     private Mbti mbti;
 
@@ -44,10 +47,12 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false, unique = true)
     private String authenticationName; //oauth2 provider + "_" + oauth2 provider id
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Article> articles = new ArrayList<>();
+
     @Builder //Builder 에서 id 를 제외하기 위해, 클래스 레벨이 아닌 생성자 레벨에 @Builder 사용
-    public User(String email, Integer age, Mbti mbti, Gender gender, Integer budget, String authenticationName) {
+    public User(String email, Mbti mbti, Gender gender, Integer budget, String authenticationName) {
         this.email = email;
-        this.age = age;
         this.mbti = mbti;
         this.gender = gender;
         this.budget = budget;
