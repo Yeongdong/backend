1c587fc65525c5e56815944fcc6796b0c13bab1f
황중섭 <jshwang9709@gmail.com>
2024-05-12 00:26:51 +0900

    comment: TODO 작성 (#4)

--- src/main/java/com/example/spinlog/user/controller/ProfileController.java
+++ src/main/java/com/example/spinlog/user/controller/ProfileController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ProfileController {
+public class ProfileController { //TODO 배포 시 삭제
 
     @GetMapping("/profile")
     public String profilePage() {
