d813607a319c5bf52a035b4f487037243082d6d2
황중섭 <jshwang9709@gmail.com>
2024-05-12 17:32:14 +0900

    refactor: 응답 스펙 변경
    ResponseEntity 사용하지 않고 ApiResponseWrapper 를 바로 반환

--- src/main/java/com/example/spinlog/user/controller/AuthenticationController.java
+++ src/main/java/com/example/spinlog/user/controller/AuthenticationController.java
@@ -2,13 +2,12 @@ package com.example.spinlog.user.controller;
 
 import com.example.spinlog.global.response.ApiResponseWrapper;
 import com.example.spinlog.global.response.ResponseUtils;
-import com.example.spinlog.user.dto.response.LoginResponseDto;
 import com.example.spinlog.global.security.oauth2.user.CustomOAuth2User;
+import com.example.spinlog.user.dto.response.LoginResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,32 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     @GetMapping("/login-result")
-    public ResponseEntity<ApiResponseWrapper<LoginResponseDto>> login(@AuthenticationPrincipal CustomOAuth2User oAuth2User) {
+    public ApiResponseWrapper<LoginResponseDto> login(@AuthenticationPrincipal CustomOAuth2User oAuth2User) {
         LoginResponseDto requestDto = LoginResponseDto.of(oAuth2User.getFirstLogin());
 
-        ApiResponseWrapper<LoginResponseDto> response;
         if (oAuth2User.getFirstLogin()) {
-            response = ResponseUtils.ok(requestDto, "회원가입에 성공했습니다.");
-        } else{
-            response = ResponseUtils.ok(requestDto, "로그인에 성공했습니다.");
+            return ResponseUtils.ok(requestDto, "회원가입에 성공했습니다.");
         }
 
-        return ResponseEntity.ok(response);
+        return ResponseUtils.ok(requestDto, "로그인에 성공했습니다.");
     }
 
     @GetMapping("/logout-result")
-    public ResponseEntity<ApiResponseWrapper<Object>> logout(HttpServletRequest request, HttpServletResponse httpResponse) {
+    public ApiResponseWrapper<Object> logout(HttpServletRequest request, HttpServletResponse httpResponse) {
         Boolean isRedirected = (Boolean) request.getSession().getAttribute("redirected");
 
         if (isRedirected != null && isRedirected) {
             request.getSession().removeAttribute("redirected");
-
-            ApiResponseWrapper<Object> response = ResponseUtils.ok("정상적으로 로그아웃되었습니다.");
-            return ResponseEntity.ok(response);
+            return ResponseUtils.ok("정상적으로 로그아웃되었습니다.");
         }
 
-        ApiResponseWrapper<Object> response = ResponseUtils.error("잘못된 접근입니다."); //TODO 리팩토링
-        return ResponseEntity.ok(response);
+        return ResponseUtils.error("잘못된 접근입니다."); //TODO 리팩토링
     }
 
 }
