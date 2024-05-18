5f6941882203467f46caeb185e4ad52f9ce38f31
HSH <han41562@gmail.com>
2024-05-18 20:18:44 +0900

    refactor: 형태소 분석을 Komoran 라이브러리에서 외부 API로 리팩토링
    build.gradle에서 komoran 의존성 삭제
    FeignClient를 이용해 API 요청
    단어 빈도수 리스트를 빈도수의 내림차순으로 상위 20개만 반환하도록 변경

--- src/main/java/com/example/spinlog/statistics/service/WordExtractionService.java
+++ src/main/java/com/example/spinlog/statistics/service/WordExtractionService.java
@@ -1,11 +1,12 @@
 package com.example.spinlog.statistics.service;
 
+import com.example.spinlog.statistics.service.dto.Token;
 import com.example.spinlog.statistics.service.dto.WordFrequency;
-import kr.co.shineware.nlp.komoran.model.Token;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -15,56 +16,49 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class WordExtractionService {
-    private final KomoranService komoranService;
+    private final MorphemeExtractionService morphemeExtractionService;
+    private final int MAX_SIZE = 20;
 
     public List<WordFrequency> analyzeWords(List<String> memos){
-        /**
-         * 1. 명사 형용사 감탄사 명사만 뽑는다
-         * 2. 명사, 형용사에 '다' 붙인다.
-         * 3. 각 단어 빈도수 정리
-         * */
+        List<Token> tokens = morphemeExtractionService.extractTokensFromMemos(memos);
+        List<String> keywords = filterAndModifyKeywords(tokens);
+        List<WordFrequency> wordFrequencies = groupByFrequency(keywords);
 
-        List<String> keyWords = getKeyWords(
-                komoranService.getTokens(memos));
-        Map<String, Long> collect = getWordFrequencies(keyWords);
-
-        return collect.entrySet()
-                .stream()
-                .map(e -> WordFrequency.builder()
-                        .word(e.getKey())
-                        .frequency(e.getValue())
-                        .build())
+        // 단어 빈도수를 빈도수 내림차순으로 정렬해서 상위 20개만 반환한다.
+        return wordFrequencies.stream()
+                .sorted(Comparator.comparingLong(WordFrequency::getFrequency).reversed())
+                .limit(MAX_SIZE)
                 .toList();
     }
 
-    private static Map<String, Long> getWordFrequencies(List<String> keyWords) {
-        return keyWords.stream()
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.counting()
-                ));
-    }
-
-    private List<String> getKeyWords(List<Token> tokens) {
+    private List<String> filterAndModifyKeywords(List<Token> tokens) {
         return tokens.stream()
-                .filter(this::isKeyWord)
-                .map(t -> {
-                    if (isVerbOrAdjective(t))
-                        return t.getMorph().concat("다");
-                    return t.getMorph();
-                })
+                .filter(this::isKeyword)
+                .map(this::getMorphWithSuffix)
+                .collect(Collectors.toList());
+    }
+
+    private List<WordFrequency> groupByFrequency(List<String> keywords) {
+        return keywords.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .map(this::createWordFrequency)
                 .toList();
     }
 
-    private boolean isKeyWord(Token token){
-        // KOMORAN 라이브러리에서 제공하는 명사, 대명사, 동사, 형용사, 외국어에 해당하는 pos
-        // https://docs.komoran.kr/firststep/postypes.html
-        return token.getPos().equals("NNG") ||
-                token.getPos().equals("NNP") ||
-                token.getPos().equals("NP") ||
-                token.getPos().equals("VV") ||
-                token.getPos().equals("VA") ||
-                token.getPos().equals("SL");
+    private WordFrequency createWordFrequency(Map.Entry<String, Long> entry) {
+        return WordFrequency.builder()
+                .word(entry.getKey())
+                .frequency(entry.getValue())
+                .build();
+    }
+
+    private String getMorphWithSuffix(Token token) {
+        return isVerbOrAdjective(token) ? token.getMorph().concat("다") : token.getMorph();
+    }
+
+    private boolean isKeyword(Token token){
+        return token.getPos().matches("NNG|NNP|NP|VV|VA|SL");
     }
 
     private boolean isVerbOrAdjective(Token token) {
