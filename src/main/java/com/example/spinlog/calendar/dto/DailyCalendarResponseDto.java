6c911bc8ec489efeaee3bb2c8cd81ca238472d1e
Yeongdong <jyd37855@gmail.com>
2024-05-20 18:22:53 +0900

    refactor: null일때 빈값 반환

--- src/main/java/com/example/spinlog/calendar/dto/DailyCalendarResponseDto.java
+++ src/main/java/com/example/spinlog/calendar/dto/DailyCalendarResponseDto.java
@@ -3,10 +3,15 @@ package com.example.spinlog.calendar.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Collections;
 import java.util.List;
 
 @Data
-@Builder
 public class DailyCalendarResponseDto {
     private List<DaySpend> daySpendList;
+
+    @Builder
+    public DailyCalendarResponseDto(List<DaySpend> daySpendList) {
+        this.daySpendList = (daySpendList != null ? daySpendList : Collections.emptyList());
+    }
 }
